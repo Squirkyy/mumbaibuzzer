e3bfa50b65be7e50e8def104b9dfa276a3766c3d
Darius Vollmer <dariusv@outlook.de>
2023-09-24 04:43:36 +0200

    added game page to enter code

---
+++ pages/game/play.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { UserButton } from 'vue-clerk';
+
+definePageMeta({
+  middleware: ['auth'],
+});
+</script>
+
+<template>
+  <main class="flex min-h-screen flex-col items-center justify-evenly p-24">
+    <div class="text-xl font-bold">Enter the game code!</div>
+    <div class="join">
+      <UserButton/>
+    </div>
+  </main>
+</template>
