562d9f4b3e5271a5ff0d95d2e7f65aa1550079e3
Darius Vollmer <dariusv@outlook.de>
2023-09-26 03:56:29 +0200

    added TRPC

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
   build: {
-    transpile: ['vue-clerk', '@clerk/clerk-js'],
+    transpile: ['trpc-nuxt', 'vue-clerk', '@clerk/clerk-js']
   },
   runtimeConfig: {
     public: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,18 @@
 <template>
   <main class="flex min-h-screen flex-col items-center justify-between p-24">
-    <h1 class="font-bold">Hey {{user?.user?.value?.firstName}}</h1>
+    <h1 class="font-bold">{{greeting}}</h1>
     <div class="flex w-full">
-      <NuxtLink to="/game/play" class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">PLAY</NuxtLink>
+      <NuxtLink
+        to="/game/play"
+        class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center"
+        >PLAY</NuxtLink
+      >
       <div class="divider divider-horizontal">OR</div>
-      <NuxtLink to="/create" class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">CREATE</NuxtLink>
+      <NuxtLink
+        to="/create"
+        class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center"
+        >CREATE</NuxtLink
+      >
     </div>
     <div>
       <UserButton afterSignOutUrl="/" />
@@ -12,9 +20,10 @@
   </main>
 </template>
 
-
 <script setup lang="ts">
-import {UserButton, useUser} from "vue-clerk";
+import { UserButton, useUser } from "vue-clerk";
+const { $client } = useNuxtApp();
 const user = useUser();
-
+const hello = await $client.hello.useQuery({ text: (user?.user?.value?.firstName)});
+const greeting = useState('greeting', () => (hello.data?.value?.greeting));
 </script>
