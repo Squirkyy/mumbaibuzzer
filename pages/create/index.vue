1d0e7468c6bfdfdb96d4722ee5e1b64e106e76ea
Darius Vollmer <dariusv@outlook.de>
2023-10-28 23:46:12 +0200

    created new create game page

---
+++ pages/create/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <h1>Create a new game</h1>
+  <form>
+    <div class="form-control w-full max-w-xs">
+      <label class="label">
+        <span class="label-text">Name: </span>
+      </label>
+      <input type="text" placeholder="Amazing game..." class="input input-bordered w-full max-w-xs" />
+    </div>
+  </form>
+</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
